a2e7ee0e36f1539524d7504e8f36c799dd1665d9
James Phelps <james.elias.phelps@gmail.com>
2024-12-19 11:51:51 -0700

    Test Changes for Double Clock in Fix

--- analog/src/app/components/clock/clock.component.ts
+++ analog/src/app/components/clock/clock.component.ts
@@ -15,7 +15,7 @@ import {
 } from "../../objects";
 import { ToastService } from "src/app/services/toast.service";
 import { ConfirmDialog } from "src/app/dialogs/confirm/confirm.dialog";
-import {ErrorDialog} from "src/app/dialogs/error/error.dialog";
+import { ErrorDialog } from "src/app/dialogs/error/error.dialog";
 import { InternationalDialog } from "src/app/dialogs/international/international.dialog";
 import { DoubleDialog } from "src/app/dialogs/double/double.dialog";
 import { SvgPreloadService } from "src/app/services/svg-preload.service";
@@ -27,6 +27,8 @@ import { SvgPreloadService } from "src/app/services/svg-preload.service";
 })
 export class ClockComponent implements OnInit {
   public punchType = PunchType;
+
+  private clockingInProgress = false;
 
   private _empRef: EmployeeRef;
   get emp(): Employee {
@@ -44,7 +46,7 @@ export class ClockComponent implements OnInit {
     public dialog: MatDialog,
     private toast: ToastService,
     public svgPreloadService: SvgPreloadService
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -75,7 +77,7 @@ export class ClockComponent implements OnInit {
           msg: "You have worked more than 15 hours this week."
         }
       })
-    }   
+    }
   }
 
   jobRef(jobID: string): BehaviorSubject<Position> {
@@ -95,16 +97,23 @@ export class ClockComponent implements OnInit {
   trimTitle = (title: string) => {
     if (title.length > 23) {
       const first = title.substring(0, 23);
-        return `${first}...`;
-    } 
+      return `${first}...`;
+    }
     return title;
   }
-
   clockInOut = (jobRef: BehaviorSubject<Position>, state: PunchType) => {
+    if (this.clockingInProgress) {
+      console.warn("Clocking already in progress. Please wait.");
+      return;
+    }
+
+    // Set the cooldown flag
+    this.clockingInProgress = true;
+
     console.log("clocking job", jobRef.value.businessTitle, "to state", state);
-  
-    //Get the Time Entry Code
-    var tec: string = null;
+
+    // Get the Time Entry Code
+    let tec: string = null;
     if (this.emp.timeEntryCodes !== null) {
       const timeEntryCodesKeys = Object.keys(this.emp.timeEntryCodes);
       tec = this.emp.timeEntryCodes[timeEntryCodesKeys[0]].id;
@@ -118,123 +127,121 @@ export class ClockComponent implements OnInit {
           }
         }
       }
-    }
-
-    //If this.emp.timeEntryCodes is null, then the employee is not eligible to track time
-    else {
+    } else {
       this.dialog.open(ErrorDialog, {
         data: {
           msg: "Employee Not Eligible for Time Tracking"
         }
-      })
-      return
+      });
+      this.clockingInProgress = false; 
+      return;
     }
 
-    //Check that other jobs are not already clocked in
-    for (var i = 0; i < this.emp.positions.length; i++) {
+    // Check that other jobs are not already clocked in
+    for (let i = 0; i < this.emp.positions.length; i++) {
       if (this.emp.positions[i].inStatus && this.emp.positions[i].positionNumber !== jobRef.value.positionNumber) {
         this.dialog.open(ErrorDialog, {
           data: {
             msg: "A Different Job is Already Clocked In"
           }
-        })
+        });
         this.refreshPage();
-        return
+        this.clockingInProgress = false; 
+        return;
       }
     }
 
-
-    //Check to see if they are double clocking
+    // Handle double clocking
     if (state === "I" && jobRef.value.inStatus) {
       this.dialog.open(DoubleDialog, {
         data: {
           msg: "Are you sure you want to clock in again?"
         }
-      }).afterClosed()
-      .subscribe(confirmed => {
-        if (confirmed === "cancel") {
-          return
-        }
-        else if (confirmed === "continue") {
-          this.sendPunch(jobRef, state, tec)
-        }
       })
+        .afterClosed()
+        .subscribe(confirmed => {
+          if (confirmed === "cancel") {
+            this.clockingInProgress = false; 
+            return;
+          } else if (confirmed === "continue") {
+            this.sendPunch(jobRef, state, tec);
+          }
+        });
     } else if (state === "O" && !jobRef.value.inStatus) {
       this.dialog.open(DoubleDialog, {
         data: {
           msg: "Are you sure you want to clock out again?"
         }
-      }).afterClosed()
-      .subscribe(confirmed => {
-        if (confirmed === "cancel") {
-          return
-        }
-        else if (confirmed === "continue") {
-          this.sendPunch(jobRef, state, tec)
-        }
       })
-    } 
-    // If it is a normal punch, send it
-    else {
-      this.sendPunch(jobRef, state, tec)
+        .afterClosed()
+        .subscribe(confirmed => {
+          if (confirmed === "cancel") {
+            this.clockingInProgress = false; 
+            return;
+          } else if (confirmed === "continue") {
+            this.sendPunch(jobRef, state, tec);
+          }
+        });
+    } else {
+      this.sendPunch(jobRef, state, tec);
     }
   };
 
-  
-sendPunch = (jobRef: BehaviorSubject<Position>, state: PunchType, tec: String) => {
-  //Construct and Send Punch Request
-  const data = new PunchRequest();
-  data.id = this.emp.id;
-  data.positionNumber = String(jobRef.value.positionNumber);
-  data.clockEventType = state === "I" ? "IN" : "OUT";
-  data.timeEntryCode = tec;
+  sendPunch = (jobRef: BehaviorSubject<Position>, state: PunchType, tec: string) => {
+    const data = new PunchRequest();
+    data.id = this.emp.id;
+    data.positionNumber = String(jobRef.value.positionNumber);
+    data.clockEventType = state === "I" ? "IN" : "OUT";
+    data.timeEntryCode = tec;
 
-  const obs = this.api.punch(data).pipe(share());
-  obs.subscribe({
-    next: (resp) => {
-      const response = JSON.parse(resp);
-      if (response.written_to_tcd === 'true') {
-        this.dialog.open(ConfirmDialog, {
-          data: {state: data.clockEventType}
-        })
-        .afterClosed()
-        .subscribe(confirmed => {
-          if (confirmed === "logout") {
-            this.logout();
-          }
-          else if (confirmed === "confirmed") {
-            this.refreshPage();
-          }
-        })
-      } else {
-        console.log(resp.written_to_tcd)
+    const obs = this.api.punch(data).pipe(share());
+    obs.subscribe({
+      next: (resp) => {
+        const response = JSON.parse(resp);
+        if (response.written_to_tcd === 'true') {
+          this.dialog.open(ConfirmDialog, {
+            data: { state: data.clockEventType }
+          })
+            .afterClosed()
+            .subscribe(confirmed => {
+              if (confirmed === "logout") {
+                this.logout();
+              } else if (confirmed === "confirmed") {
+                this.refreshPage();
+              }
+            });
+        } else {
+          console.log(resp.written_to_tcd);
+          this.refreshPage();
+          this.dialog.open(ErrorDialog, {
+            data: {
+              msg: "The Punch was not Submitted Successfully"
+            }
+          });
+        }
+      },
+      error: (err) => {
         this.refreshPage();
+        console.warn("response ERROR", err);
         this.dialog.open(ErrorDialog, {
           data: {
             msg: "The Punch was not Submitted Successfully"
           }
-        })
+        });
+      },
+      complete: () => {
+        this.clockingInProgress = false; 
       }
-      
-    },
-    error: (err) => {
-      this.refreshPage();
-      console.warn("response ERROR", err);
-      this.dialog.open(ErrorDialog, {
-        data: {
-          msg: "The Punch was not Submitted Successfully"
-        }
-      })
-      
-    }
-  });
-};
+    });
+  };
+
 
   toTimesheet = () => {
     this._empRef.selectedDate = new Date();
-    this.router.navigate(["./date/"], { 
+    this.router.navigate(["./date/"], {
       relativeTo: this.route,
-      queryParamsHandling: "preserve" });
+      queryParamsHandling: "preserve"
+    });
   };
 
   logout = () => {
@@ -243,8 +250,10 @@ sendPunch = (jobRef: BehaviorSubject<Position>, state: PunchType, tec: String) =
 
   refreshPage = () => {
     this.router.navigate([], {
-      queryParams: {theme: this.api.theme == "dark" ? "dark" : 
-      this.api.theme == "default" ? "light" : "default"},
+      queryParams: {
+        theme: this.api.theme == "dark" ? "dark" :
+          this.api.theme == "default" ? "light" : "default"
+      },
       queryParamsHandling: "merge"
     });
   }
